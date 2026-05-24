5829417cd88f57c5ed7b9c558019512bdc99c22b
Tibor Ukropina <tiborvukropina@gmail.com>
2026-05-24 15:00:34 -0300

    Initial project setup: Spring Boot API and Angular UI

---
+++ marketplace-api/src/main/java/com/example/MarketplaceClone/MarketplaceCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarketplaceClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceCloneApplication.class, args);
+	}
+
+}
---
+++ marketplace-api/src/test/java/com/example/MarketplaceClone/MarketplaceCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarketplaceClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
